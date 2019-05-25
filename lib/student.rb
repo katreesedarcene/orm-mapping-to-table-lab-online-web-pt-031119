3560b824bef1b40f7a3b9d70863a265735bb2e1c
Kat B <katreesebarnes@gmail.com>
2019-05-25 00:12:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,5 @@
 class Student
-  attr_accessor :name 
+  attr_accessor :name, :grade, :id = nil
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
