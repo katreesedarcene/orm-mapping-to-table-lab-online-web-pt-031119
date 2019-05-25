1b4086da06b4b46c71fd558f8e4fd21e23b1bb23
Kat B <katreesebarnes@gmail.com>
2019-05-25 00:13:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,12 @@
 class Student
   attr_accessor :name, :grade, :id 
   
-  def initialize(name, grade, id = nil)
+  def initialize(name, grade, id=nil)
+    
+    
+    
+    
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
