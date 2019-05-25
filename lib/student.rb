a21788fad292248b07ed90dd3755d218eb5f22ff
Kat B <katreesebarnes@gmail.com>
2019-05-25 00:17:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,8 +9,14 @@ class Student
     
   end
   def self.create_table 
-    
-    
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
     
   end
 
