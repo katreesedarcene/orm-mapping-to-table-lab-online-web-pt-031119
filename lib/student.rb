2db3262076300274d6e72f37f60cd59ce04985de
Kat B <katreesebarnes@gmail.com>
2019-05-25 00:29:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,15 +23,15 @@ class Student
     sql = "DROP TABLE IF EXISTS students;"
      DB[:conn].execute(sql) 
   end
+  
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade)
+      INSERT INTO students (name)
       VALUES (?, ?)
     SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, self.name)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-
   end
 
   # Remember, you can access your database connection anywhere in this class
